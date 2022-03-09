ad07c6f90bf956921ed75812fc10007d25a235a0
Elimar Alves <elimaralves_@hotmail.com>
2022-03-09 19:50:02 -0300

    Configurando Banco parte 2

--- source/LEIAMAIS.business/obj/Release/netcoreapp3.1/LEIAMAIS.business.AssemblyInfo.cs
+++ source/LEIAMAIS.business/obj/Release/netcoreapp3.1/LEIAMAIS.business.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LEIAMAIS.Business")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LEIAMAIS.business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("LEIAMAIS.Business")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LEIAMAIS.Business")]
+[assembly: System.Reflection.AssemblyProductAttribute("LEIAMAIS.business")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LEIAMAIS.business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
