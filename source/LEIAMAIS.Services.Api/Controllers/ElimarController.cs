0487ef08d02d58b5dee8ba13cb0dbaa8cb4772a4
Elimar Alves <elimaralves_@hotmail.com>
2022-03-09 00:03:42 -0300

    Iniciando uma CRUD WEB API

---
+++ source/LEIAMAIS.Services.Api/Controllers/ElimarController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LEIAMAIS.Services.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ElimarController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Elimar A";
+        }
+
+        [HttpGet("{idade}")]
+        public string Get(string idade)
+        {
+            string nomeComIdade = $"Elimar A : {idade}";
+            return nomeComIdade;
+        }
+
+        [HttpPost("{teste}")]
+        public string Post(string teste)
+        {
+            return "Chegou o Post";
+        }
+
+        [HttpPut]
+        public string Put(string teste)
+        {
+            return "Ñ";
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            
+        }
+    }
+}
